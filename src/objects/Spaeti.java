a4c0f7c49e8746c465ea30400aade2c66365b999
Tom Oberhauser <tom@devfoo.de>
2017-01-21 16:36:58 +0100

    Späti got direction. Closes #15.

--- src/objects/Spaeti.java
+++ src/objects/Spaeti.java
@@ -1,10 +1,20 @@
 package objects;
 
+import utils.GameUtils;
+
 /**
  * Created by tom on 19.01.17.
  */
 public class Spaeti extends House {
-    public Spaeti(int pos_x, int pos_y) {
+
+    private GameUtils.Direction direction;
+
+    public Spaeti(int pos_x, int pos_y, GameUtils.Direction direction) {
         super(pos_x, pos_y);
+        this.direction = direction;
+    }
+
+    public GameUtils.Direction getDirection() {
+        return direction;
     }
 }
