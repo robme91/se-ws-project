43ad9764df736d1a228896ddc8e7867f00ab7f1c
Tom Oberhauser <tom@devfoo.de>
2017-01-22 17:21:34 +0100

    Späti Asset and new DemoLevel

--- src/objects/Spaeti.java
+++ src/objects/Spaeti.java
@@ -1,6 +1,9 @@
 package objects;
 
 
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
 /**
  * Created by tom on 19.01.17.
  */
@@ -11,6 +14,11 @@ public class Spaeti extends House {
     public Spaeti(int pos_x, int pos_y, Enums.Direction direction) {
         super(pos_x, pos_y);
         this.direction = direction;
+        try {
+            this.image = new Image("/res/img/objects/spaeti.png");
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
     }
 
     public Enums.Direction getDirection() {
