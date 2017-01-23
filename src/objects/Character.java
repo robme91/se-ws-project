64a5b19903d6c89d301da54631a30b48aeccbab7
Tom Oberhauser <tom@devfoo.de>
2017-01-23 18:29:50 +0100

    Generate the right image for every direction. Closes #36.

--- src/objects/Character.java
+++ src/objects/Character.java
@@ -1,6 +1,11 @@
 package objects;
 
+import org.newdawn.slick.Image;
 import org.newdawn.slick.geom.Rectangle;
+import utils.GameUtils;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by tom on 13.01.17.
@@ -12,6 +17,8 @@ public abstract class Character extends GameObject {
     private Enums.Direction direction = null;
 
     protected String name;
+
+    private Map<Enums.Direction, Image> directedImages = null;
 
     public Character(int pos_x, int pos_y, boolean isBlocking, float speed) {
         super(pos_x, pos_y, 20, isBlocking);
@@ -37,6 +44,11 @@ public abstract class Character extends GameObject {
         this.hitbox.setCenterY(y);
     }
 
+    @Override
+    public Image getImage() {
+
+        return super.getImage();
+    }
 
     public Enums.Direction getDirection() {
         return direction;
@@ -44,5 +56,29 @@ public abstract class Character extends GameObject {
 
     public void setDirection(Enums.Direction direction) {
         this.direction = direction;
+
+        // if first call, generate directed images
+        if (this.directedImages == null) {
+            fillDirectedImages();
+        }
+        // make sure that images does not change when character stops
+        if (direction != null) {
+            this.image = directedImages.get(direction);
+        }
     }
+
+    /**
+     * Since Images get set within concrete class, we have to fill the Map at first query.
+     */
+    private void fillDirectedImages() {
+        // create directed images
+        this.directedImages = new HashMap<>(5);
+        this.directedImages.put(null, this.image);
+        for (Enums.Direction d : Enums.Direction.values()) {
+            Image img = this.image.copy();
+            img.rotate(GameUtils.getImageRotationFromDirection(d));
+            this.directedImages.put(d, img);
+        }
+    }
+
 }
