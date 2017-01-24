f78293af2e515510aca1d499b39234d629040138
Tom Oberhauser <tom@devfoo.de>
2017-01-24 22:29:34 +0100

    Documentation and refactoring of Spaeti

--- src/objects/Spaeti.java
+++ src/objects/Spaeti.java
@@ -7,15 +7,32 @@ import org.newdawn.slick.geom.Line;
 import utils.GameUtils;
 
 /**
- * Created by tom on 19.01.17.
+ * Class for Spaeti to drink beer
  */
 public class Spaeti extends House {
 
+    /**
+     * Door direction, Spaeti can only be accessed from this direction
+     */
     private Enums.Direction direction;
 
+    /**
+     * Image if Spaeti is open
+     */
     private Image image_on;
+
+    /**
+     * Image if Spaeti is closed
+     */
     private Image image_off;
 
+    /**
+     * Creates a new Spaeti
+     *
+     * @param pos_x     center X
+     * @param pos_y     center Y
+     * @param direction direction of door
+     */
     public Spaeti(int pos_x, int pos_y, Enums.Direction direction) {
         super(pos_x, pos_y);
         this.direction = direction;
@@ -31,14 +48,17 @@ public class Spaeti extends House {
         }
     }
 
+    /**
+     * Handles player beer recharge
+     *
+     * @param go object that hit this object
+     */
     @Override
     public void interact(GameObject go) {
         if (interactionTimeout == 0) {
             if (go.getClass().equals(Player.class)) {
-                Line target = new Line(this.getPos_x(), this.getPos_y(), go.getPos_x(), go
-                        .getPos_y());
-                Enums.Direction touchDirection = GameUtils.getDirectionFromXY(target.getDX(),
-                        target.getDY());
+                Line target = new Line(this.getPos_x(), this.getPos_y(), go.getPos_x(), go.getPos_y());
+                Enums.Direction touchDirection = GameUtils.getDirectionFromXY(target.getDX(), target.getDY());
                 if (touchDirection == this.direction) {
                     this.interactionTimeout = this.rechargeDuration;
                     Player p = (Player) go;
@@ -50,11 +70,11 @@ public class Spaeti extends House {
         }
     }
 
-    @Override
-    public Image getImage() {
-        return super.getImage();
-    }
-
+    /**
+     * Handles image switching between off and on
+     *
+     * @param ms time since last call in ms (should be around 1000)
+     */
     @Override
     public void secondTick(int ms) {
         if (this.interactionTimeout == 0) {
@@ -63,6 +83,11 @@ public class Spaeti extends House {
         super.secondTick(ms);
     }
 
+    /**
+     * Get direction of door
+     *
+     * @return direction of Spaeti door
+     */
     public Enums.Direction getDirection() {
         return direction;
     }
