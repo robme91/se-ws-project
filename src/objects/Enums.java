633e994ca8e704d3824c699a66a95f21e5212777
Tom Oberhauser <tom@devfoo.de>
2017-01-24 22:56:25 +0100

    Documentation and refactoring of Enums.

--- src/objects/Enums.java
+++ src/objects/Enums.java
@@ -1,18 +1,27 @@
 package objects;
 
 /**
- * Created by tom on 21.01.17.
+ * Enums
  */
 public class Enums {
 
+    /**
+     * Enum to specify the attack target of an npc
+     */
     public enum AttackTarget {
         DRINK, PLAYER_SPEED
     }
 
+    /**
+     * Enum for directions
+     */
     public enum Direction {
         UP, DOWN, LEFT, RIGHT
     }
 
+    /**
+     * Enum for stret types
+     */
     public enum StreetType {
         SOLO, STRAIGHT, END, BEND, CROSS3, CROSS4
     }
